0a65b2a6531f0fb204366d12aeca40fc00c7ece4
MacAnderson Uche <macandersonuche@yahoo.com>
2023-09-05 21:33:51 +0100

    added submit feedback

--- feedback/src/submitfeedback/app.ts
+++ feedback/src/submitfeedback/app.ts
@@ -1,13 +1,15 @@
 import { APIGatewayProxyEvent, APIGatewayProxyResult } from "aws-lambda";
+import { handleSubmit } from ".";
 
 export const lambdaHandler = async (
   event: APIGatewayProxyEvent
 ): Promise<APIGatewayProxyResult> => {
+  const returned = await handleSubmit(JSON.parse(event.body!));
   try {
     return {
       statusCode: 200,
       body: JSON.stringify({
-        message: "hello world",
+        message: returned,
       }),
     };
   } catch (err) {
