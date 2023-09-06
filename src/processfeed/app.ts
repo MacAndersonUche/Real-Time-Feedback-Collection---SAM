0a7ccf9d0cddea1b91eb4161724e91e6cb75966a
MacAnderson Uche <macandersonuche@yahoo.com>
2023-09-06 22:16:29 +0100

    working on email sender

--- src/processfeed/app.ts
+++ src/processfeed/app.ts
@@ -1,15 +1,14 @@
-import {
-  APIGatewayProxyEvent,
-  APIGatewayProxyResult,
-  DynamoDBStreamEvent,
-} from "aws-lambda";
-import { handleSNS } from ".";
+import { APIGatewayProxyResult, SQSEvent } from "aws-lambda";
+import { handleSQS } from ".";
 import { shapeErrorResponse, shapeResponse } from "../utils/reponse";
 
 export const lambdaHandler = async (
-  event: DynamoDBStreamEvent
+  event: SQSEvent
 ): Promise<APIGatewayProxyResult> => {
-  const result = await handleSNS(event.Records[0].dynamodb?.NewImage);
+  console.log({ event, body: event.Records[0].body });
+
+  const body = JSON.parse(event.Records[0].body);
+  const result = await handleSQS(body);
 
   try {
     return shapeResponse(200, { data: result });
--- src/processfeed/index.ts
+++ src/processfeed/index.ts
@@ -1,70 +1,53 @@
-import { Feedback } from "../types";
+import { readFileSync } from "fs";
 const AWS = require("aws-sdk");
-export const handleSNS = async (body: any) => {
-  //extract email address from object and publish to SNS with a message
-  // the sns message should be a text saying they have recieved it.
-  //the subscription to sns resource will be a single email address
+export const handleSQS = async (body: any) => {
+  console.log({ body, message: body.message });
 
-  const dynamoDbItem = parseBody(body);
+  const details = extractEmailAndName(body.message);
+  const html = getTemplate();
 
-  console.log({ dynamoDbItem });
-
-  await publishToSNS(dynamoDbItem as unknown as Feedback);
-
-  /*
-
-  log
-  a function that takes the following 
-   newItem: {
-    feedbacktext: { S: 'Test1' },
-    author: { S: 'OSAS' },
-    id: { S: 'f53a4b29-8656-4e25-97a6-c02e8a6629f1' },
-    email: { S: 'mac1@email.com' },
-    status: { S: 'Submited' }
-  }, and returns an object
-   */
+  await sendEmail(details.email, "", "THANKS FOR SIGNING UP", html);
 };
 
-interface Val {
-  [key: string]: string;
+interface Extracted {
+  id: string;
+  email: string;
+  name: string;
 }
-function parseBody(body: any) {
-  //return array of objects
-  //  const object1 = {
-  //    a: "somestring",
-  //    b: 42,
-  //  };
-  const bodyToReturn: Record<string, string> = {};
-  for (const [key, value] of Object.entries(body)) {
-    console.log(`${key}: ${value}`);
-    if (value) {
-      let choiceItem = value as Val;
-      bodyToReturn[key] = choiceItem["S"];
-    }
-  }
-
-  return bodyToReturn;
+function extractEmailAndName({ id, email, name }: Extracted) {
+  return {
+    id,
+    email,
+    name,
+  };
 }
 
-async function publishToSNS(body: Feedback) {
-  const TOPIC_ARN = process.env.TOPIC_ARN!;
-  // Create publish parameters
-  var params = {
-    Message: body /* required */,
-    TopicArn: TOPIC_ARN,
+function getTemplate(): string {
+  return readFileSync("src/processfeed/welcome.html", "utf-8");
+}
+
+async function updateDynamoDBStatus(id: string) {}
+
+async function sendEmail(
+  toAddress: string,
+  fromAddress: string,
+  subject: string,
+  body: string
+) {
+  const ses = new AWS.SES({ region: "eu-west-3" });
+
+  const params = {
+    Destination: {
+      ToAddresses: [toAddress],
+    },
+    Message: {
+      Body: {
+        Html: { Data: body },
+      },
+      Subject: { Data: subject },
+    },
+    Source: fromAddress,
   };
 
-  // Create promise and SNS service object
-  const publishTextPromise = new AWS.SNS({ apiVersion: "2010-03-31" })
-    .publish(params)
-    .promise();
-
-  publishTextPromise
-    .then(function (data: { MessageId: string }) {
-      console.log(`Message sent to the topic ${params.TopicArn}`);
-      console.log("MessageID is " + data.MessageId);
-    })
-    .catch(function (err: { stack: any }) {
-      console.error(err, err.stack);
-    });
+  return ses.sendEmail(params).promise();
 }
